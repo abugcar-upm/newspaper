9de403fd6aa9163d0496a025faf56347a46b049d
aiden1614 <aiden.bugarin.carreira@gmail.com>
2026-09-17 15:40:20 +0200

    add bootstrap

--- Newspaper/src/app/articles-list/articles-list.ts
+++ Newspaper/src/app/articles-list/articles-list.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {Article} from '../interfaces/article';
+import { NgClass } from '@angular/common';
 
 @Component({
-  imports: [FormsModule],
+  imports: [FormsModule, NgClass],
   selector: 'app-articles-list',
   styleUrl: './articles-list.css',
   templateUrl: './articles-list.html',
@@ -11,4 +12,20 @@ import {Article} from '../interfaces/article';
 
 export class ArticlesList {
   article!: Article; // ask why we have to put the !
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.article = {
+      title: 'title',
+      subtitle: '',
+      body: '',
+      abstract: '',
+      category: '',
+    };
+  }
+
+  sendForm(): void {
+    window.alert("Received information: " + this.article.title + " " + this.article.subtitle);
+  }
 }
