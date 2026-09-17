30394305500dc61377251d42f48477039a8b177f
aiden1614 <aiden.bugarin.carreira@gmail.com>
2026-09-17 14:59:59 +0200

    add component

---
+++ Newspaper/src/app/articles-list/articles-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-articles-list',
+  styleUrl: './articles-list.css',
+  templateUrl: './articles-list.html',
+})
+export class ArticlesList {}
